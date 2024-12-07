8effdcbe9d3b8ae494e93bcf5257fcfbcf1882c5
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-12-07 23:29:16 +0530

    Update Details.js

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -51,8 +51,6 @@ const Details = () => {
   const navigate = useNavigate();
   const state = useSelector((state) => state.details);
     const { movieId } = useParams();
-  const dispatch = useDispatch();
-
 
 
 
