eae14fdab813cf2aa90203a1191e625e5d3742d2
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-05-03 23:24:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import "./App.css";
-import Home from "./components/home/Home";
-import { Routes, Route } from "react-router-dom";
-import Movies from "./components/movies/Movies";
-import Watchlist from "./components/watchlist/Watchlist";
-import Contatct from "./components/contatct/Contatct";
-import About from "./components/about/About";
-import Error from "./components/error/Error";
-import Search from "./components/search/Search";
-import Details from "./components/details/Details";
-import { useEffect, useState } from "react";
-import PreLoader from "./components/loading/PreLoader";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
+Import the necessary CSS and components from the specified files.
 
+```javascript
+import “./App.css”;
+import Home from “./components/home/Home”;
+import { Routes, Route } from “react-router-dom”;
+import Movies from “./components/movies/Movies”;
+import Watchlist from “./components/watchlist/Watchlist”;
+import Contact from “./components/contact/Contact”;
+import About from “./components/about/About”;
+import Error from “./components/error/Error”;
+import Search from “./components/search/Search”;
+import Details from “./components/details/Details”;
+import { useEffect, useState } from “react”;
+import PreLoader from “./components/loading/PreLoader”;
+import Footer from “./components/footer/Footer”;
+import Navbar from “./components/navbar/Navbar”;
+```
+
+Define the App component and set up the state for page loading.
+
+```javascript
 function App() {
   const [pageLoading, setPageLoading] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
       setPageLoading(false);
@@ -29,26 +37,53 @@ function App() {
         <>
           <Navbar />
           <Routes>
-            <Route path="/MovieInfoApp" element={<Home />} />
-            <Route path="/MovieInfoApp/movies" element={<Movies />} />
+            <Route path=“/MovieInfoApp” element={<Home />} />
+            <Route path=“/MovieInfoApp/movies” element={<Movies />} />
             <Route
-              path="/MovieInfoApp/movies/:movieId"
+              path=“/MovieInfoApp/movies/:movieId”
               element={<Details />}
             />
-            <Route path="/MovieInfoApp/watchlist" element={<Watchlist />} />
-            <Route path="/MovieInfoApp/search" element={<Search />} />
-            <Route path="/MovieInfoApp/about" element={<About />} />
-            <Route path="/MovieInfoApp/contact" element={<Contatct />} />
-            <Route
-              path="/MovieInfoApp/*"
-              element={<Error msg="Page Not Found" btn="true" height={100} />}
-            />
+            <Route path=“/MovieInfoApp/watchlist” element={<Watchlist />} />
+            <Route path=“/MovieInfoApp/search” element={<Search />} />
+            <Route path=“/MovieInfoApp/about” element={<About />} />
           </Routes>
-          <Footer />
         </>
       )}
     </>
   );
 }
+```
 
-export default App;
+Render the content based on the page loading state.
+
+```javascript
+return (
+  <>
+    {pageLoading ? (
+      <PreLoader />
+    ) : (
+      <>
+        <Navbar />
+        <Routes>
+          <Route path=“/MovieInfoApp” element={<Home />} />
+          <Route path=“/MovieInfoApp/movies” element={<Movies />} />
+          <Route
+            path=“/MovieInfoApp/movies/:movieId”
+            element={<Details />}
+          />
+          <Route path=“/MovieInfoApp/watchlist” element={<Watchlist />} />
+          <Route path=“/MovieInfoApp/search” element={<Search />} />
+          <Route path=“/MovieInfoApp/about” element={<About />} />
+        </Routes>
+      </>
+    )}
+  </>
+);
+```
+<Route path=“/MovieInfoApp/contact” element={<Contact />} />
+<Route path=“/MovieInfoApp/*” element={<Error msg=“Page Not Found” btn=“true” height={100} />} />
+</Routes>
+<Footer />
+</>
+```
+
